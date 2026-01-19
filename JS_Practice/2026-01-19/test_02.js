fdd664d8f88342c9f10b376cc2762068c24c34fe
Jeremy <ag.cyho@gmail.com>
2026-01-19 20:04:18 +0800

    feat: add exam

---
+++ JS_Practice/2026-01-19/test_02.js
@@ -0,0 +1,20 @@
+async function test() {
+  console.log(1)
+
+  const result = await getValue()
+
+  console.log(result)
+  console.log(4)
+}
+
+function getValue() {
+  console.log(2)
+
+  return Promise.resolve(3)
+}
+
+console.log(0)
+test()
+console.log(5)
+
+// 請寫出輸出順序
