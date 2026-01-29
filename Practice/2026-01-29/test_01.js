863a37b069ccb45c79bf11eb3ce85e1bc613691c
Jeremy <ag.cyho@gmail.com>
2026-01-29 19:58:26 +0800

    feat: add exam and course

---
+++ Practice/2026-01-29/test_01.js
@@ -0,0 +1,15 @@
+// 以下是一個來自 Lorem Picsum (https://picsum.photos/) 的 API
+//
+// API：
+// https://picsum.photos/v2/list
+//
+// 請使用「純 HTML + JavaScript」完成以下功能：
+//
+// 1. 頁面載入時，顯示五張圖片
+// 2. 點擊按鈕後，重新隨機顯示五張圖片
+// 3. 圖片請使用 API response 中「可直接作為 <img src> 的圖片檔 URL」顯示
+// 4. 請使用 axios 並搭配 async/await
+// 5. 請顯示圖片作者（author 欄位）
+
+
+const apiUrl = 'https://picsum.photos/v2/list'
